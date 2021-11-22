50505296fe85af355784c6ec9fa92df206f4e962
Leonardo Kasperavičius <leocck@gmail.com>
2021-11-22 19:52:30 +0100

    Define position property

--- src/main/java/com/redi/j2/UIComponent.java
+++ src/main/java/com/redi/j2/UIComponent.java
@@ -1,5 +1,16 @@
 package com.redi.j2;
 
+import java.awt.*;
+
 public abstract class UIComponent {
 
+    private Point position;
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
 }
